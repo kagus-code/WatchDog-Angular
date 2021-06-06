fea44cb9391ac44a4f777b95e2a391a549d4ecc2
Eston Kagwima <ekagwima745@gmail.com>
2021-06-06 20:47:15 +0300

    adding login service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {LoginService} from '../login-service/login.service'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService]
 })
 export class LoginComponent implements OnInit {
+  input:any;
 
-  constructor() { }
+  constructor(private loginservice:LoginService) { }
 
   ngOnInit(): void {
+    this.input = {
+      username: '',
+      password:''
+    };
+  }
+
+  onLogin(){
+    this.loginservice.loginUser(this.input).subscribe(
+      response => {
+        alert('User ' + this.input.username + ' has been loged in')
+        this.input.username = '',
+        this.input.password = '',
+      },
+      error => console.log('error', error)
+    );
+  
   }
 
 }
