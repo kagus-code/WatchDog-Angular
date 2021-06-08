0637935c4dc0c44e99429359ef6dadfbd890b9a1
Eston Kagwima <ekagwima745@gmail.com>
2021-06-08 15:26:23 +0300

    changing api url from local to deployed

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -6,7 +6,7 @@ import {Observable} from 'rxjs'
   providedIn: 'root'
 })
 export class SharedService {
-  readonly APIUrl = "http://127.0.0.1:8000"
+  readonly APIUrl = "https://kagus-watchdog.herokuapp.com"
 
   constructor(private http:HttpClient) { }
   gethood():Observable<any[]>{
